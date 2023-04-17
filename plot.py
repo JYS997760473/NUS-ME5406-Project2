06b0437bdae043f2323ce646bf3ce6d7fd659bad
JYS997760473 <3057737959@qq.com>
2023-04-17 22:17:44 +0800

    add plot result part

---
+++ plot.py
@@ -0,0 +1,13 @@
+from src.utilities import *
+import argparse
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp_name', type=str, default='4.17.12:36', 
+                        help='name of current experiment')
+    args = parser.parse_args()
+    # plot reward
+    plot_reward(exp_name=args.exp_name)
